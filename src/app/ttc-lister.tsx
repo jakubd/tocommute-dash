3cd8763001520109872e0ea8c4f6d728a4de3941
Jakub Dalek <jakub.dalek@gmail.com>
2025-02-17 19:22:10 -0500

    small renames

--- src/app/ttc-lister.tsx
+++ src/app/ttc-lister.tsx
@@ -19,19 +19,19 @@ export default function TTCLister({givenTTR}: {givenTTR?: TTCApiRoot}) {
             <div className="mx-auto m-4 flex max-w-3xl items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
             <div>
                 <ul>
-                    <span>Site Wide: {TTR.siteWideCustom.length? TTR.siteWideCustom.length : 0}</span>
+                    <span>Site Wide Alerts: {TTR.siteWideCustom.length? TTR.siteWideCustom.length : 0}</span>
                     <div className="mx-4">
                         {TTR.siteWideCustom.map((item, idx) => (
                            <p key={idx}><span className="font-bold">{item.severity}</span>: {item.description}</p> 
                         ))}
                     </div>
-                    <span>Total Routes: {TTR.routes.length}</span>
+                    <span>Routes Alerts: {TTR.routes.length}</span>
                     <div className="mx-4">
                         {TTR.routes.map((item, idx) => (
                            <p key = {idx}><span className="font-bold">{item.severity}</span>: {item.route} : {item.title}</p> 
                         ))}
                     </div>
-                    <span>Total Accessiblity: {TTR.accessibility.length}</span>
+                    <span>Accessiblity Alerts: {TTR.accessibility.length}</span>
                     <div className="mx-4">
                         {TTR.accessibility.map((item, idx) => (
                            <span key = {idx}><span className="font-bold">{item.severity}</span>: {item.title}</span> 
