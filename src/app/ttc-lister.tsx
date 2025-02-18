19a8a4017d26403cddbbf1fc0235ad6a8689c6a8
Jakub Dalek <jakub.dalek@gmail.com>
2025-02-17 19:37:30 -0500

    fancier route callouts

--- src/app/ttc-lister.tsx
+++ src/app/ttc-lister.tsx
@@ -22,15 +22,16 @@ export default function TTCLister({givenTTR}: {givenTTR?: TTCApiRoot}) {
                     <span>Site Wide Alerts: {TTR.siteWideCustom.length? TTR.siteWideCustom.length : 0}</span>
                     <div className="mx-4">
                         {TTR.siteWideCustom.map((item, idx) => (
-                           <p key={idx}><span className="font-bold">{item.severity}</span>: {item.description}</p> 
+                           <p key={idx}><span className="font-bold">{item.severity}</span>: <span className="border-2 border-solid rounded-md bg-red-600 m-1">{item.route}</span> {item.description}</p> 
                         ))}
                     </div>
                     <span>Routes Alerts: {TTR.routes.length}</span>
                     <div className="mx-4">
                         {TTR.routes.map((item, idx) => (
-                           <p key = {idx}><span className="font-bold">{item.severity}</span>: <span className="border-2 border-solid">{item.route}</span> {item.title}</p> 
+                           <p key = {idx}><span className="font-bold">{item.severity}</span>: <span className="border-2 border-solid rounded-md bg-red-600 m-1">{item.route}</span> {item.title}</p> 
                         ))}
                     </div>
+                    <li>source: <a href="https://www.ttc.ca/">ttc.ca</a></li>
                 </ul>
             </div>
             </div>
