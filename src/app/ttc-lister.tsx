baf1400a1dbb9d39c789b0bad6a0a674db70c51c
Jakub Dalek <jakub.dalek@gmail.com>
2025-02-20 21:29:42 -0500

    no log

--- src/app/ttc-lister.tsx
+++ src/app/ttc-lister.tsx
@@ -55,8 +55,7 @@ export default function TTCLister({givenTTR}: {givenTTR?: TTCApiRoot}) {
 
         const TTR = structuredClone(givenTTR);
         const rts = filterRoutes(SimplifyRouteData(TTR.routes));
-        console.log(rts);
-
+        
         return(
             <div className="mx-auto m-4 flex max-w-3xl items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
             <div>
