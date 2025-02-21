9426f3adbbbeee787ccd30b51ff96ce9d689fa7e
Jakub Dalek <jakub.dalek@gmail.com>
2025-02-20 21:31:25 -0500

    count should account for the ghost WEBSITE alert so lets subtract one

--- src/app/ttc-lister.tsx
+++ src/app/ttc-lister.tsx
@@ -55,12 +55,12 @@ export default function TTCLister({givenTTR}: {givenTTR?: TTCApiRoot}) {
 
         const TTR = structuredClone(givenTTR);
         const rts = filterRoutes(SimplifyRouteData(TTR.routes));
-        
+
         return(
             <div className="mx-auto m-4 flex max-w-3xl items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
             <div>
                 <ul>
-                    <span>Routes Alerts: {TTR.routes.length}</span>
+                    <span>Route Alerts: {TTR.routes.length - 1}</span>
                     <div className="mx-4">
                         {rts.map((item, idx) => (
                            <p key = {idx}><span className="font-bold">{item.severity}</span>: <span className="border-2 border-solid rounded-md bg-red-600 m-1">{item.route}</span> {item.title}</p> 
