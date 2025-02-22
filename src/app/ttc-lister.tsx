3af5fb2fb078338a2f20d6202245a10b3a15d799
Jakub Dalek <jakub.dalek@gmail.com>
2025-02-22 11:01:27 -0500

    Fix count of route alerts

--- src/app/ttc-lister.tsx
+++ src/app/ttc-lister.tsx
@@ -18,7 +18,7 @@ export default function TTCLister({givenRoutes}: {givenRoutes?: Array<RouteAlert
             <div className="mx-auto m-4 flex max-w-3xl items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
             <div>
                 <ul>
-                    <span>Route Alerts: {givenRoutes.length - 1}</span>
+                    <span>Route Alerts: {givenRoutes.length}</span>
                     <div className="mx-4">
                         {givenRoutes.map((item, idx) => (
                            <p key = {idx}><span className="font-bold">{item.severity}</span>: <span className="border-2 border-solid rounded-md bg-red-600 m-1">{item.route}</span> {item.title}</p> 
